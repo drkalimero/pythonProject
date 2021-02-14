3b31405fd3f37d5119c225be45c1fe804c8ddc82
drkalimero <liron951>
2021-02-15 01:32:43 +0200

    a

--- טסט/טסט1.py
+++ טסט/טסט1.py
@@ -1,1 +1,19 @@
 print("Hello Kalimero")
+
+a=11
+b=12.1
+c="kalimer"
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
+
+name="liron"
+age=24
+
+print("hello " + name + "\nhow are you?\nyou age is: " + str(age))
